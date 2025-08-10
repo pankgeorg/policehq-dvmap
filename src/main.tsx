8d149773d35addeb675330991ee23e43f219ef7e
Panagiotis Georgakopoulos <panagiotis.georgakopoulos@juliahub.com>
2025-08-10 12:14:35 +0300

    chore: re-format

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,15 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-)
+);
 
-if(window.top !== window.self){
-  const root = document.getElementById('root')
-  if(root) root.classList.add("embedded")
+if (window.top !== window.self) {
+  const root = document.getElementById("root");
+  if (root) root.classList.add("embedded");
 }
